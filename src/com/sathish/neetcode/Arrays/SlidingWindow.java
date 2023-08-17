32da1215d2e7b6c0441fc5fb7efd390c18bd3922
sathish <64584022+sathishbvb@users.noreply.github.com>
2023-08-17 08:21:42 +0530

    new hard problem not completed

--- src/com/sathish/neetcode/Arrays/SlidingWindow.java
+++ src/com/sathish/neetcode/Arrays/SlidingWindow.java
@@ -1,13 +1,17 @@
 package com.sathish.neetcode.Arrays;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Arrays;
 
 public class SlidingWindow {
     public static void main(String[] args) {
         SlidingWindow sw = new SlidingWindow();
 //        System.out.println(sw.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(sw.characterReplacement("ABABBA",1));
+//        System.out.println(sw.characterReplacement("ABABBA",1));
+//        System.out.println(sw.checkInclusion("ab","eidbaooo"));
+        System.out.println(sw.minWindow("ADOBECODEBANC","ABC"));
     }
 
     public int lengthOfLongestSubstring1(String s) {
@@ -87,5 +91,53 @@ public class SlidingWindow {
         return max;
     }
 
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int[] freq = new int[26];
+        int m = s2.length();
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int[] freq2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            if (i >= n) {
+                freq2[s2.charAt(i - n) - 'a']--;
+            }
+            if (Arrays.equals(freq, freq2))
+                return true;
+        }
+        return false;
+    }
 
+
+//    Given two strings s and t of lengths m and n respectively, return the minimum window substring
+//    of s such that every character in t (including duplicates) is included in the window. If there is no such substring,
+//    return the empty string "".
+    public String minWindow(String s, String t) {
+        if(s.length()<t.length()){
+            return "";
+        }
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        HashMap<Character,Integer> sMap = new HashMap<>();
+        for(char c : t.toCharArray()){
+            tMap.put(c,tMap.getOrDefault(s,0)+1);
+        }
+        int i=0,j=0;
+        String str=s+t;
+        while(j<s.length()){
+            while(j<s.length()&& !tMap.equals(sMap)){
+                if(tMap.containsKey(s.charAt(j))){
+                    sMap.put(s.charAt(j),sMap.getOrDefault(s.charAt(j),0)+1);
+                }
+                j++;
+            }
+            String s1 = s.substring(i,j);
+            str=str.length()>s1.length()?s1:str;
+            sMap=new HashMap<>();
+            i++;
+            j=i;
+        }
+        return str;
+    }
 }
